e4e38c238afa4bc1bf6213d5d2bec5d7165234e1
Dani Ponce Mendoza <danniscreamer@gmail.com>
2023-11-21 18:06:42 -0600

    se completó el ejercicio de las rutas

--- routes/libros.js
+++ routes/libros.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const router = express.Router();
 
-
-// router.get('/', (req,res)=>{
-//     res.send("Libros")
-// })
-
  const libros = []
 
  router.get('/', (req,res)=>{
@@ -25,5 +20,42 @@ const router = express.Router();
        res.send("Error en almacenar la petición")
      }
  })
+
+ // Ruta PUT
+
+ router.put('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const { autor, editorial, año } = req.body;
+  const libro = libros.find((l) => l.id === id);
+  console.log("Libro",libro)
+  
+  if (!libro) {
+    // El libro no existe
+    res.send("Error al actualizar el libro");
+  } else {
+    // El libro existe
+    libro.autor = autor;
+    libro.editorial = editorial;
+    libro.año = año;
+    console.log(libro);
+    res.json("Petición recibida");
+  }
+  
+ });
+ 
+ // Ruta DELETE
+ 
+ router.delete('/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   const libro = libros.find((l) => l.id === id);
+   if (libro) {
+    //splice() cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
+    //ndexOf() retorna el primer índice en el que se puede encontrar un elemento dado en el array, ó retorna -1 si el elemento no esta presente.
+     libros.splice(libros.indexOf(libro), 1);
+     res.json("Petición recibida");
+   } else {
+     res.send("Error al eliminar el libro");
+   }
+ });
 
 module.exports = router
