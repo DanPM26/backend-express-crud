49a0a65eed32fefdefe531b40cc052d9d105fbe1
Dany Ponce <danniscreamer@gmail.com>
2024-02-03 11:01:46 -0600

    se añadió la dumentación de Swagger

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUI = require("swagger-ui-express")
+const swaggerJsDoc = require("swagger-jsdoc")
+const path = require("path")
 require('dotenv').config()
 const app = express()
 
 const PORT = 5000
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Node API for Order Management',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: PORT,
+            },
+        ],
+    },
+    // Ruta de los archivos donde se buscarán los comentarios para generar la documentación
+    apis: [`${path.join(__dirname, './routes/index.js')}`],
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+
 app.use(cors())
 app.use(express.json())
+
+
 const {librosRouter} = require('./routes')
 
 console.log(process.env.VERCEL_FORCE_NO_BUILD_CACHE)
+
 app.use('/api', librosRouter)
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.get('/', (req,res)=>{
     res.send("Hola mundo")
--- routes/libros.js
+++ routes/libros.js
@@ -2,10 +2,88 @@ const express = require('express')
 const router = express.Router();
 
  const libros = []
+ 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Order:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The order's unique identifier
+ *        autor:
+ *          type: string
+ *          description: Nombre del autor
+ *        editorial:
+ *          type: string
+ *          description: Nombre de la editorial
+ *        año:
+ *          type: number
+ *          description: Año de la publicación
+ *        total:
+ *          type: number
+ *          description: The order's total amount
+ *      required:
+ *        - id
+ *        - nombre
+ *        - editorial
+ *        - año
+ *      example:
+ *        id: 1
+ *        autor: "Raewyn Connel"
+ *        editorial: "CIEG"
+ *        año: 2015
+ */
+
+// a. Obtener pedido
+/**
+ * @swagger
+ * /api:
+ *  get:
+ *    summary: Obtener todos los libros
+ *    tags: [Orders]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Order'
+ *    responses:
+ *      201:
+ *        description: Se obtuvieron todos los datos
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ */
 
  router.get('/', (req,res)=>{
  res.json(libros)
  })
+
+ // b. Crear pedido
+/**
+ * @swagger
+ * /api:
+ *  post:
+ *    summary: Añadir libros
+ *    tags: [Orders]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Order'
+ *    responses:
+ *      201:
+ *        description: Se accedieron correctamente los libros
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ */
 
  router.post('/', (req,res)=>{
      const {autor,editorial,año} = req.body;
@@ -21,7 +99,27 @@ const router = express.Router();
      }
  })
 
- // Ruta PUT
+  // b. Crear pedido
+/**
+ * @swagger
+ * /api/{id}:
+ *  put:
+ *    summary: Editar libros
+ *    tags: [Orders]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Order'
+ *    responses:
+ *      201:
+ *        description: Se accedieron correctamente los libros
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ */
 
  router.put('/:id', (req, res) => {
   const id = parseInt(req.params.id);
@@ -43,6 +141,27 @@ const router = express.Router();
   
  });
  
+  // b. Crear pedido
+/**
+ * @swagger
+ * /api/{id}:
+ *  delete:
+ *    summary: Borrar libros especificos
+ *    tags: [Orders]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Order'
+ *    responses:
+ *      201:
+ *        description: Se borró correctamente el libro
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ */
  // Ruta DELETE
  
  router.delete('/:id', (req, res) => {
